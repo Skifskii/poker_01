070ce47e7839acf145acc573d27bef5f116946ec
Skifskii <petya-koreshkov@yandex.ru>
2021-06-18 17:55:21 +0300

    no

--- poker_10/poker_10.py
+++ poker_10/poker_10.py
@@ -12,27 +12,42 @@ Config.set('graphics', 'width', '315')
 Config.set('graphics', 'height', '560')
 
 
-def callback(instance):
-    print('esssss')
-
-
 class MyApp(App):
+
+
+    def add_num(self,instance):
+        print(int(instance.text))
+
+
     def build(self):
         rows = BoxLayout(orientation = 'vertical') #, padding = [10, 30, 10, 450])
-        num = GridLayout(cols = 3, spacing = 10, padding = [50, 60, 50, 150])
-        #five_seven = BoxLayout(orientation = 'horizontal', spacing = 10, padding = [50])
+        suits = BoxLayout(orientation = 'horizontal', spacing = 10, padding = [30], size_hint = (1, .20))
+        num = GridLayout(cols = 3, spacing = 10, padding = [50, 0], size_hint = (1, .4))
+        j_a = BoxLayout(orientation = 'horizontal', spacing = 10, padding = [30], size_hint = (1, .20))
         #eight_ten = BoxLayout(orientation = 'horizontal', spacing = 10, padding = [50])
 
         rows.add_widget(Label(text = 'Сколько игроков за столом?', font_size = 20, size_hint=(1, .3)))
 
+        suits.add_widget(Button(text = 'ch', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+        suits.add_widget(Button(text = 'bu', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+        suits.add_widget(Button(text = 'kr', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+        suits.add_widget(Button(text = 'pi', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+
+        rows.add_widget(suits)
+
         for i in range(2, 11):
             btn1 = Button(text = str(i), font_size = 27, background_color = [1, 0, 1, 1])
-            btn1.bind(on_press=callback)
+            btn1.bind(on_press = self.add_num)
             num.add_widget(btn1)
             
-
         rows.add_widget(num)
+        
+        j_a.add_widget(Button(text = 'J', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+        j_a.add_widget(Button(text = 'Q', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+        j_a.add_widget(Button(text = 'K', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
+        j_a.add_widget(Button(text = 'A', font_size = 27, background_color = [1, 0, 1, 1], on_press = self.add_num))
 
+        rows.add_widget(j_a)
         '''for i in range(5, 8):
            five_seven.add_widget(Button(text = str(i)))
 
